895fe928e7401614d6be1eb17f03ac27c49219c9
1-C-E <pitenevv@mail.ru>
2024-04-15 21:59:53 +0300

    checking the entered data in the registration field

--- pages/register.vue
+++ pages/register.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { z } from 'zod'
-import type { FormSubmitEvent } from '#ui/types'
+import {z} from 'zod'
+import type {FormSubmitEvent} from '#ui/types'
 
 const {register} = useFirebaseAuth()
+
+
 const schema = z.object({
   email: z.string().email('Invalid email'),
   password: z.string().min(6, 'Must be at least 6 characters')
@@ -15,23 +17,24 @@ const state = reactive({
   password: undefined
 })
 
-async function onSubmit (event: FormSubmitEvent<Schema>) {
+async function onSubmit(event: FormSubmitEvent<Schema>) {
   try {
     await register(event.data.email, event.data.password)
   } catch (error) {
-
+    console.log(error)
   }
 }
+
 </script>
 
 <template>
   <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
     <UFormGroup label="Email" name="email">
-      <UInput v-model.trim="state.email" />
+      <UInput v-model.trim="state.email"/>
     </UFormGroup>
 
     <UFormGroup label="Password" name="password">
-      <UInput v-model.trim="state.password" type="password" />
+      <UInput v-model.trim="state.password" type="password"/>
     </UFormGroup>
 
     <UButton type="submit">
